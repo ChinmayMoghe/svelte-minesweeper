7d4d350ce3ea7c8eb3df0e7fb31dbf59eccfb575
chinmay@wsl <>
2024-07-28 13:38:55 +0530

    Fix linting issues

--- public/timer.worker.js
+++ public/timer.worker.js
@@ -1,36 +1,35 @@
 let intervalId;
 
 function startTimer({ interval = 1000 }) {
-  if (intervalId) {
-    stopTimer();
-  } else {
-    postMessage('TICK');
-    intervalId = setInterval(() => {
-      postMessage('TICK');
-    }, interval);
-  }
-};
+	if (intervalId) {
+		stopTimer();
+	} else {
+		postMessage("TICK");
+		intervalId = setInterval(() => {
+			postMessage("TICK");
+		}, interval);
+	}
+}
 
 function stopTimer() {
-  if (intervalId) {
-    clearInterval(intervalId);
-    intervalId = undefined;
-  }
+	if (intervalId) {
+		clearInterval(intervalId);
+		intervalId = undefined;
+	}
 }
 
 function handleMainThreadMessage({ data }) {
-  const { type, payload } = data;
-  switch (type) {
-    case 'START_TIMER':
-      startTimer(payload);
-      break;
-    case 'STOP_TIMER':
-      stopTimer();
-      break;
-    default:
-      break;
-  }
-};
+	const { type, payload } = data;
+	switch (type) {
+		case "START_TIMER":
+			startTimer(payload);
+			break;
+		case "STOP_TIMER":
+			stopTimer();
+			break;
+		default:
+			break;
+	}
+}
 
 self.onmessage = handleMainThreadMessage;
-
--- src/lib/client/interfaces/GameInterfaces.ts
+++ src/lib/client/interfaces/GameInterfaces.ts
@@ -1,64 +1,64 @@
 export enum CellType {
-  empty,
-  mine,
-  count
+	empty = 0,
+	mine = 1,
+	count = 2,
 }
 
 export enum CellClickState {
-  clicked,
-  not_clicked,
+	clicked = 0,
+	not_clicked = 1,
 }
 export enum CellSymbols {
-  mine = "💣",
-  flag = "🚩",
-  empty = "",
-  explode = "💥",
+	mine = "💣",
+	flag = "🚩",
+	empty = "",
+	explode = "💥",
 }
 
 export enum FlagState {
-  flagged,
-  not_flagged,
+	flagged,
+	not_flagged,
 }
 
 export type Cell = {
-  text: string;
-  row: number;
-  col: number;
-  type: CellType;
-  clicked: CellClickState;
-  flagged: FlagState;
+	text: string;
+	row: number;
+	col: number;
+	type: CellType;
+	clicked: CellClickState;
+	flagged: FlagState;
 };
 
 export type Bound = {
-  row_min: number;
-  row_max: number;
-  col_min: number;
-  col_max: number;
+	row_min: number;
+	row_max: number;
+	col_min: number;
+	col_max: number;
 };
 
 export enum ClickType {
-  left = 0,
-  right = 2,
+	left = 0,
+	right = 2,
 }
 
 export enum GameState {
-  on,
-  win,
-  lose,
+	on = 0,
+	win = 1,
+	lose = 2,
 }
 
 export type GameConfig = {
-  rows: number;
-  cols: number;
-  mines: number;
-  cellSize: string;
+	rows: number;
+	cols: number;
+	mines: number;
+	cellSize: string;
 };
 
 export type GameModes = "baby" | "boy" | "expert" | "gambler";
 
 export const GameDifficulty: Record<GameModes, GameConfig> = {
-  baby: { rows: 5, cols: 5, mines: 2, cellSize: "40px" }, // 8% board covered with mines
-  boy: { rows: 9, cols: 9, mines: 10, cellSize: "30px" }, // 12% covered with mines
-  expert: { rows: 16, cols: 16, mines: 40, cellSize: "27px" }, // 15% covered with mines
-  gambler: { rows: 16, cols: 30, mines: 120, cellSize: "25px" }, // 25% covered with mines - u need to be only lucky to beat this
+	baby: { rows: 5, cols: 5, mines: 2, cellSize: "40px" }, // 8% board covered with mines
+	boy: { rows: 9, cols: 9, mines: 10, cellSize: "30px" }, // 12% covered with mines
+	expert: { rows: 16, cols: 16, mines: 40, cellSize: "27px" }, // 15% covered with mines
+	gambler: { rows: 16, cols: 30, mines: 120, cellSize: "25px" }, // 25% covered with mines - u need to be only lucky to beat this
 };
